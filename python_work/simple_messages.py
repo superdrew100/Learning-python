4d4dfbb5fb1317324638d9339f9ff26e3ec7f576
Drew Thomasson <superdrew100@Gmail.com>
2021-06-18 17:46:40 -0400

    created simple_messages.py and made it print a message variable 3 times and each time it prints it I changed the value thats assigned to that variable to make it print diffrent message the next time

---
+++ python_work/simple_messages.py
@@ -0,0 +1,6 @@
+message= "This is a simepl message to be rewritten over as a diffrent message."
+print(message)
+message="This message variable has been assigned to a diffrent message and this message is that new message."
+print(message)
+message="This is a even newer message ot be assigned to this variable to be printed"
+print(message)
