1295f29c31efe285c137cb7aa4e87ae3619fa407
Drew Thomasson <superdrew100@Gmail.com>
2021-06-19 17:46:10 -0400

    creaded adding_comments.py and made a couple of simple programs which i then put a comment above of discribing that the program does

---
+++ python_work/adding_comments.py
@@ -0,0 +1,7 @@
+#this program prints the value assigned to number
+number=5
+print(number)
+#This program prints out the the statments with teh included values assigned ot the variable within it
+name,fav_num,fav_color="Drew",16,'red'
+message=f'Hi, my name is {name}.\nMy favorite number is {fav_num}!\nAnd my favorite color is {fav_color}\nHave a good day!'
+print(message)
