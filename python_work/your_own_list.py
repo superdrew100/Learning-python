09c2175eb4054970a2fa5678107111d0ef9c31c6
Drew Thomasson <superdrew100@Gmail.com>
2021-06-19 19:00:50 -0400

    created your_own_list.py and made a list to be sused to display a item in a list in string.

---
+++ python_work/your_own_list.py
@@ -0,0 +1,16 @@
+transport_modes=["motercycle", "bus", "train"]
+companys=["Honda", "Sudan", "Tesla"]
+message= f"I would like to own a {companys[0]} {transport_modes[0]} in the potential future."
+print(message)
+message= f"I would like to own a {companys[1]} {transport_modes[0]} in the potential future."
+print(message)
+message= f"I would like to own a {companys[2]} {transport_modes[0]} in the potential future."
+print(message)
+message= f"I would like to own a {companys[1]} {transport_modes[1]} in the potential future."
+print(message)
+message= f"I would like to own a {companys[1]} {transport_modes[2]} in the potential future."
+print(message)
+message= f"I would like to own a {companys[1]} {transport_modes[0]} in the potential future."
+print(message)
+message= f"I would like to own a {companys[2]} {transport_modes[2]} in the potential future."
+print(message)
