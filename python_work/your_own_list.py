6bc8c9113b4cd8fbb86878beaa45b53e24481b58
Drew Thomasson <superdrew100@Gmail.com>
2021-06-19 19:18:04 -0400

    modified your_own_list.py so the company names are capitalized using the .title() method

--- python_work/your_own_list.py
+++ python_work/your_own_list.py
@@ -1,16 +1,19 @@
 transport_modes=["motercycle", "bus", "train"]
 companys=["Honda", "Sudan", "Tesla"]
-message= f"I would like to own a {companys[0]} {transport_modes[0]} in the potential future."
+message= f"I would like to own a {companys[0].title()} {transport_modes[0]} in the potential future."
 print(message)
-message= f"I would like to own a {companys[1]} {transport_modes[0]} in the potential future."
+message= f"I would like to own a {companys[1].title()} {transport_modes[0]} in the potential future."
 print(message)
-message= f"I would like to own a {companys[2]} {transport_modes[0]} in the potential future."
+message= f"I would like to own a {companys[2].title()} {transport_modes[0]} in the potential future."
 print(message)
-message= f"I would like to own a {companys[1]} {transport_modes[1]} in the potential future."
+message= f"I would like to own a {companys[1].title()} {transport_modes[1]} in the potential future."
 print(message)
-message= f"I would like to own a {companys[1]} {transport_modes[2]} in the potential future."
+message= f"I would like to own a {companys[1].title()} {transport_modes[2]} in the potential future."
 print(message)
-message= f"I would like to own a {companys[1]} {transport_modes[0]} in the potential future."
+message= f"I would like to own a {companys[1].title()} {transport_modes[0]} in the potential future."
 print(message)
-message= f"I would like to own a {companys[2]} {transport_modes[2]} in the potential future."
+message= f"I would like to own a {companys[2].title()} {transport_modes[2]} in the potential future."
 print(message)
+butts=["this","jack"]
+butts.remove('jack')
+print(butts)
