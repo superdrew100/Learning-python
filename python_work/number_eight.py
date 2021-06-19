6c912982150f41fad27c820a9f07231732b922b6
Drew Thomasson <superdrew100@Gmail.com>
2021-06-19 17:36:09 -0400

    Made a bunch of simple number math problems that resulted with 8 as the answer

---
+++ python_work/number_eight.py
@@ -0,0 +1,7 @@
+# all of these will result in 8 when run
+print(4+4)
+print(12-4)
+print(2*4)
+#I did a round to the d9vsion one so then it wouldnt be displayed as a flaot as the result
+print(round(64/8))
+print(2**3)
