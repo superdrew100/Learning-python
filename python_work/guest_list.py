86e89e6105f0bb67761d6049f095777dbef167b0
Drew Thomasson <superdrew100@Gmail.com>
2021-06-22 20:28:59 -0400

    created guest_list.py and made a guest list where I use the values inserted into the list within a statment inviting each of them to dinner.

---
+++ python_work/guest_list.py
@@ -0,0 +1,4 @@
+guest_list=["jhon", "james bond", "abraham lincon"]
+print(f'Dear, {guest_list[0].title()}, I would like to invite you to a dinner party.')
+print(f'Dear, {guest_list[1].title()}, I would like to invite you to a dinner party.')
+print(f'Dear, {guest_list[2].title()}, I would like to invite you to a dinner party.')
