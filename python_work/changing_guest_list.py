00760c6f799126037e17b52703fcd00ab61d9747
Drew Thomasson <superdrew100@Gmail.com>
2021-06-22 20:38:08 -0400

    modified the list using pop and printed a message saying that teh person i popped off the list was unable to arrive at the party, and then i sent out invitatiosn again to the people who still can come to the party

---
+++ python_work/changing_guest_list.py
@@ -0,0 +1,8 @@
+guest_list=["jhon", "james bond", "abraham lincon"]
+print(f'Dear, {guest_list[0].title()}, I would like to invite you to a dinner party.')
+print(f'Dear, {guest_list[1].title()}, I would like to invite you to a dinner party.')
+print(f'Dear, {guest_list[2].title()}, I would like to invite you to a dinner party.')
+couldt_arrive= guest_list.pop(1)
+print(f"I'm sorry to have to mention but {couldt_arrive} was unable to ariive at the dinner party.")
+print(f'Dear, {guest_list[0].title()}, I would like to invite you to a dinner party.')
+print(f'Dear, {guest_list[1].title()}, I would like to invite you to a dinner party.')
